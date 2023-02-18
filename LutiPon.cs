e916e7932fff0315d72e955c92f7fffa7ca32d18
capybara13 <125746622+capybara13@users.noreply.github.com>
2023-02-18 19:58:40 +0500

    Add files via upload

---
+++ LutiPon.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LutiPon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //вывод на консоль чел тебе надо выбраться из танка бабаха
+        print("чел те нада выбрася и танка бабаха");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("crickets");
+           
+    }
+}
